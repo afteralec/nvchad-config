0814bebe0f78febc92a0f32e5489c6730488586a
Alec DuBois <after.alec@gmail.com>
2023-10-20 18:56:55 -0700

    Actually remove autocmd

--- init.lua
+++ init.lua
@@ -1,13 +1,1 @@
 vim.opt.colorcolumn = "80"
-
-local autocmd_group_formatter =
-  vim.api.nvim_create_augroup("FormatterAutoCommandGroup", { clear = true })
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.lua", "*.go" },
-  desc = "Auto-format on save",
-  callback = function()
-    vim.cmd ":Format"
-  end,
-  group = autocmd_group_formatter,
-})
